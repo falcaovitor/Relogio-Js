07242b456de29626d36a6fda95fcf6c5d076b08d
falcaovitor <detonabit@gmail.com>
2020-02-11 16:51:55 -0300

    Update relogio.js
    Adicionando if ao script

--- scripts/relogio.js
+++ scripts/relogio.js
@@ -4,7 +4,41 @@ function relogio(){
 	var hora = data.getHours()
 	var minuto = data.getMinutes()
 	var segundo = data.getSeconds()
+	if(hora < 10){
+		hora = "0"+hora
+	}
+	if(minuto < 10){
+		minuto = "0"+minuto
+	}
+	if (segundo < 10) {
+		segundo = "0"+segundo
+	}
 	var formato = hora+" : "+minuto+" : "+segundo
 	inner.innerHTML = formato
-	setTimeout("relogio()", 1000)
+	
+	// setTimeout("relogio()",1000)
+	
 }
+window.setInterval("relogio()",1000)
+
+// /*Relógio com if*/
+// function relogio(){
+// 	var data = new Date()
+// 	var hora = data.getHours()
+// 	var minuto = data.getMinutes()
+// 	var segundo = data.getSeconds()
+
+// 	if (hora < 10) {
+// 		hora = "0"+hora
+// 	}
+// 	if (minuto < 10) {
+// 		minuto = "0"+minuto
+// 	}
+// 	if (segundo < 10) {
+// 		segundo = "0"+segundo
+// 	}
+
+// 	document.getElementById("time").innerHTML = hora" : "+minuto+" : "+segundo
+// }
+// window.setInterval("relogio()",1000)
+
