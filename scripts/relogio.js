e098a6d5d57ef2788257f67d6c88f311243b11a0
Vitor Falcão <51137534+falcaovitor@users.noreply.github.com>
2021-02-25 00:29:11 -0300

    Update relogio.js

--- scripts/relogio.js
+++ scripts/relogio.js
@@ -4,41 +4,24 @@ function relogio(){
 	var hora = data.getHours()
 	var minuto = data.getMinutes()
 	var segundo = data.getSeconds()
-	if(hora < 10){
+	if (hora <= 10) 
+	{
 		hora = "0"+hora
 	}
-	if(minuto < 10){
+	if (minuto <= 10) 
+	{
 		minuto = "0"+minuto
 	}
-	if (segundo < 10) {
+	if (segundo <= 10) 
+	{
 		segundo = "0"+segundo
 	}
-	var formato = hora+" : "+minuto+" : "+segundo
-	inner.innerHTML = formato
-	
-	// setTimeout("relogio()",1000)
-	
+	var formato = `${hora} : ${minuto} : ${segundo}`
+	inner.innerText = formato
+	setInterval("relogio()", 1000)
+	// trabalhando o dia da semana
+	var show = document.querySelector('#day')
+	var dayNames = ['Domingo','Segunda-feira','terça-feira','Quarta-feira','Quinta-feira','Sexta-feira','Sábado']
+	var day = dayNames[data.getDay()]
+	show.innerText = day
 }
-window.setInterval("relogio()",1000)
-
-// /*Relógio com if*/
-// function relogio(){
-// 	var data = new Date()
-// 	var hora = data.getHours()
-// 	var minuto = data.getMinutes()
-// 	var segundo = data.getSeconds()
-
-// 	if (hora < 10) {
-// 		hora = "0"+hora
-// 	}
-// 	if (minuto < 10) {
-// 		minuto = "0"+minuto
-// 	}
-// 	if (segundo < 10) {
-// 		segundo = "0"+segundo
-// 	}
-
-// 	document.getElementById("time").innerHTML = hora" : "+minuto+" : "+segundo
-// }
-// window.setInterval("relogio()",1000)
-
